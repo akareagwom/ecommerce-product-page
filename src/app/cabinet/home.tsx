368f74b0db9de2d07130558c456c4dc5684f2a06
akareagwom <103905242+akareagwom@users.noreply.github.com>
2024-02-23 22:19:56 +0100

    mobile carousel

--- src/app/cabinet/home.tsx
+++ src/app/cabinet/home.tsx
@@ -1,11 +1,13 @@
 "use client"
 import { Box, Card, CardHeader } from "@chakra-ui/react";
 import NavBar from "../components/navbar";
+import Carousel from "../components/carousel";
 
 const LandingPage = () => {
     return ( 
         <Box>
             <NavBar/>
+            <Carousel/>
         </Box>
      );
 }
---
+++ src/app/components/carousel.tsx
@@ -0,0 +1,34 @@
+import { Box } from "@chakra-ui/react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+const Carousel = () => {
+    return ( 
+        <Box>
+            <Swiper
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={50}
+            slidesPerView={3}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+    </Swiper>
+        </Box>
+     );
+}
+ 
+export default Carousel
