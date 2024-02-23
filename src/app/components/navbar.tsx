5ec9dab979e148a3a83e16da8f3da1afc960d409
akareagwom <103905242+akareagwom@users.noreply.github.com>
2024-02-23 20:14:38 +0100

    mobile navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,21 +1,32 @@
-import { Box, Image, Tab, TabList, Tabs, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, Tab, TabList, Tabs, Text } from "@chakra-ui/react";
 
 const NavBar = () => {
     return ( 
-        <Box>
-            <Image src="images/icon-menu.svg" alt="Logo" mb={5} />
-            <Text>sneakers</Text>
-            <Tabs variant={'none'}>
+        <Flex 
+        justifyContent={'space-around'}
+        alignItems={'baseline'}
+        py={'5%'}
+        boxShadow={'0 4px 8px 0 rgba(0, 0, 0, 0.2)'}
+        >
+            <Flex alignItems={'baseline'} width={'35%'} justifyContent={'space-between'}>
+                <Image src="images/icon-menu.svg" alt="Logo" mb={5} />
+                <Text fontWeight={700} fontSize={25}>sneakers</Text>
+            </Flex>
+            <Tabs display={'none'} variant={'none'}>
                 <TabList>
-                    <Tab></Tab>
-                    <Tab></Tab>
-                    <Tab></Tab>
-                    <Tab></Tab>
-                    <Tab></Tab>
+                    <Tab>Collections</Tab>
+                    <Tab>Men</Tab>
+                    <Tab>Women</Tab>
+                    <Tab>About</Tab>
+                    <Tab>Contact</Tab>
                 </TabList>
             </Tabs>
-        </Box>
+            <Flex alignItems={'center'} w={'20%'} justifyContent='space-between'>
+                <Image src="images/icon-cart.svg" />
+                <Image src="images/image-avatar.png" w={'30px'} />
+            </Flex>
+        </Flex>
      );
 }
  
-export default NavBar;
+export default NavBar
