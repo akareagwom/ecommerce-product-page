0305762b0d3ea84391d594cbf521e39f29777c14
akareagwom <akareazikang20@gmail.com>
2024-07-02 12:26:49 -0700

    carouselData

---
+++ src/app/data/carouselData.tsx
@@ -0,0 +1,18 @@
+const carouselData = [
+    {
+        slideItem:'images/image-product-1.jpg',
+        thumbItem:''
+    },
+    {
+        slideItem:'images/image-product-2.jpg',
+        thumbItem:''
+    },
+    {
+        slideItem:'images/image-product-3.jpg',
+        thumbItem:''
+    },
+    {
+        slideItem:'images/image-product-4.jpg',
+        thumbItem:''
+    },
+]
